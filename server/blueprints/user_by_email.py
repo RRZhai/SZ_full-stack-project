fa07a8584c21bf6c2fe4a3d06f22efdca92e8d75
Shiyao Zhai <zsshiyaozhai@gmail.com>
2023-07-12 13:45:36 -0700

    in progress

---
+++ server/blueprints/user_by_email.py
@@ -0,0 +1,14 @@
+from blueprints import Resource, Blueprint, make_response, g, abort 
+from models import db
+from models.user import User
+from schemas.user_schema import UserSchema
+
+user_schema = UserSchema()
+user_by_name_bp = Blueprint("user_by_name", __name__, 
+                                    url_prefix="/users/<string:username>")
+
+class UserByName(Resource):
+    def get(self, email):
+        if user := User.query.filter(User.email == email).first(): 
+            return make_response(user_schema.dump(user), 200)
+        return make_response({"error": "User not found"}, 404)
