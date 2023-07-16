db4439b98e3c67335be40969fafa04620f3601e7
RRZhai <126899363+RRZhai@users.noreply.github.com>
2023-07-16 14:07:56 -0700

    message shows on page

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -9,7 +9,6 @@ import {
   Divider,
 } from "@mui/material";
 import Box from "@mui/material/Box";
-import Avatar from "@mui/material/Avatar";
 import { useState } from "react";
 import { ChatMessageDto } from "../models/chatMessageDto";
 import Job from "./Job";
@@ -20,23 +19,23 @@ const Chat = ({ currentUser, job }) => {
   ]);
 
   const listChatMessages = messages.map((chatMessageDto, index) => {
-    <Avatar
-      alt={currentUser?.username}
-      src={`../${currentUser.profile_pic_num}.png`}
-    />;
+    return (<ListItem key={index}>
+      <ListItemText
+        primary={`${chatMessageDto.user}: ${chatMessageDto.message}`}
+      />
+    </ListItem>)
   });
 
+  const WebSocket = require("ws");
   return (
     <Container>
       <Paper elevation={5}>{job ? <Job job={job}></Job> : null}</Paper>
-      <Paper elevation={10}>
-        <Box p={10}>
-          <Typography variant="h6" align="center" color="primary">
-            Please enter your message
-          </Typography>
-          <Divider />
+      <Paper elevation={5}>
+        <Box p={3}>
           <Grid container spacing={4}>
-            {listChatMessages}
+            <Grid>
+              <List>{listChatMessages}</List>
+            </Grid>
           </Grid>
         </Box>
       </Paper>
