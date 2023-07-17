995dbb94b897a8659dab9617ce1e21873fae7ab2
RRZhai <126899363+RRZhai@users.noreply.github.com>
2023-07-17 13:24:54 -0700

    useContext setted up

--- client/src/components/Reviews.js
+++ client/src/components/Reviews.js
@@ -1,7 +1,46 @@
+import { useContext } from "react";
+import { ReviewContext } from "../context/reviewContext";
+import StarIcon from "@mui/icons-material/Star";
+
+import {
+  Container,
+  Grid,
+  Card,
+  CardActionArea,
+  CardContent,
+  Typography,
+  Rating
+} from "@mui/material";
+
 const Review = () => {
-    return(
-        <div className='review'></div>
-    )
-}
+  const { reviews } = useContext(ReviewContext);
+  
+  return (
+    <Container>
+      <Grid>
+        {reviews?.map((review) => (
+          <CardActionArea>
+            <Card sx={{ display: "flex" }}>
+              <CardContent sx={{ flex: 1 }}>
+                <Rating
+                  name="text-feedback"
+                  value={review.rating}
+                  readOnly
+                  precision={0.5}
+                  emptyIcon={
+                    <StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />
+                  }
+                />
+                <Typography variant="subtitle1" paragraph>
+                  {review.content}
+                </Typography>
+              </CardContent>
+            </Card>
+          </CardActionArea>
+        ))}
+      </Grid>
+    </Container>
+  );
+};
 
 export default Review;
--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./components/App";
+import { ReviewProvider } from "./context/reviewContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
+  <ReviewProvider>
     <BrowserRouter>
-        <App />
+      <App />
     </BrowserRouter>
-
+  </ReviewProvider>
 );
